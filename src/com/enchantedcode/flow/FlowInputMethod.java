de07e839d9e2657f6e39f528fdcf8c98f5725ad3
peastman <peastman@78243414-fca9-4e5f-95a8-b8f20ac8ecd0>
2014-08-26 04:44:22 +0000

    Improved autocompletion after pressing backspace

--- src/com/enchantedcode/flow/FlowInputMethod.java
+++ src/com/enchantedcode/flow/FlowInputMethod.java
@@ -171,13 +171,14 @@ public class FlowInputMethod extends InputMethodService
   public void onUpdateSelection(int oldSelStart, int oldSelEnd, int newSelStart, int newSelEnd, int candidatesStart, int candidatesEnd)
   {
     super.onUpdateSelection(oldSelStart, oldSelEnd, newSelStart, newSelEnd, candidatesStart, candidatesEnd);
-    boolean jumped = (newSelStart != oldSelStart+1 && touchListener.getCandidatesType() != TouchListener.CandidatesType.Trace);
+    TouchListener.CandidatesType candidatesType = touchListener.getCandidatesType();
+    boolean jumped = (newSelStart != oldSelStart+1 && candidatesType != TouchListener.CandidatesType.Trace);
     selectionStart = newSelStart;
     selectionEnd = newSelEnd;
     updateShiftMode();
     if (candidatesEnd != -1 && (newSelStart != candidatesEnd || newSelEnd != candidatesEnd))
       touchListener.selectCandidate(0, true);
-    if (candidatesEnd == -1 && (touchListener.getCandidates() == null || touchListener.getCandidatesType() == TouchListener.CandidatesType.ExistingWord || jumped))
+    if (candidatesEnd == -1 && (candidatesType != TouchListener.CandidatesType.PrefixAfterDelete || newSelStart != oldSelStart-1) && (touchListener.getCandidates() == null || candidatesType == TouchListener.CandidatesType.ExistingWord || jumped))
       touchListener.suggestReplacementsForExistingWord();
   }
 
